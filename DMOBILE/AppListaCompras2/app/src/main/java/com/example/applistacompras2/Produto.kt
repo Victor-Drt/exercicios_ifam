a7e68ed6af286d13173aba11f8445a6d15b8935e
Victor Duarte <2020003498@ifam.edu.br>
2021-11-03 23:13:48 -0400

    delete directory DMOBILE

--- DMOBILE/AppListaCompras2/app/src/main/java/com/example/applistacompras2/Produto.kt
+++
@@ -1,5 +0,0 @@
-package com.example.applistacompras2
-
-import android.graphics.Bitmap
-
-data class Produto(val nome: String, val quantidade: Int, val valor: Double, val foto: Bitmap? = null)
--- DMOBILE/AppListaCompras2/app/src/main/java/com/example/applistacompras2/Utils.kt
+++
@@ -1,7 +0,0 @@
-package com.example.applistacompras2
-
-import java.lang.Exception
-
-
-val produtosGlobal = mutableListOf<Produto>()
-
